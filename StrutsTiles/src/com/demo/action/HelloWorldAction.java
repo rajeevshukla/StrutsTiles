caeea2325a96d393f1a57a06474f27365a3aebf3
rajeevshukla <mail2rajeevshukla@gmail.com>
2014-09-03 17:28:34 +0530

    @ParentPackage added

--- StrutsTiles/src/com/demo/action/HelloWorldAction.java
+++ StrutsTiles/src/com/demo/action/HelloWorldAction.java
@@ -1,12 +1,13 @@
 package com.demo.action;
 
 import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.ResultPath;
 
 @Namespace("/")
 @ResultPath("/")
-//@ParentPackage("com")
+@ParentPackage("tiles-default")
 @Result(name="success",type="tiles", location="student.home")
 public class HelloWorldAction {
   
--- StrutsTiles/src/com/demo/action/RegisterStudentAction.java
+++ StrutsTiles/src/com/demo/action/RegisterStudentAction.java
@@ -2,10 +2,11 @@ package com.demo.action;
 
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 
 @Namespace("/")
-
+@ParentPackage("tiles-default")
 public class RegisterStudentAction {
 
 	
